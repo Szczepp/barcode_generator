791be47c5a2dbd5096a5a9a94bf8d6c3b06ce7f3
Szczepan <szczep.kowalik@gmail.com>
2022-03-12 22:24:20 +0100

    migrations and Barcode model

---
+++ app/Models/Barcode.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Barcode extends Model
+{
+    protected $table = 'barcode_records';
+
+    public $timestamps = false;
+}
---
+++ database/migrations/2022_03_12_202333_create_barcode_records.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barcode_records', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->text('text');
+            $table->text('barcode');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barcode_records');
+    }
+};
